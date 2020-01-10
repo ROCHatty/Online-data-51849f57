9944fb916515cf9f4084fe9b92db6a23e0a0a3be
Aaron Jonk <2119311@talnet.nl>
2020-01-10 09:43:44 +0100

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,14 @@
+var data = {
+	Naam: "Zara",
+	Score: 36,
+	BeginDatum: "21 Januari 2016",
+	Vrienden: [
+		"Mika",
+		"Josef",
+		"Maria",
+		"Sumail",
+		"Lotte"
+	],
+	Gekwalificeerd: true
+}
+console.log(data);
